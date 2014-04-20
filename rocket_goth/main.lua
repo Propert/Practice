06cf5d58ba59d96d820d21c7a7073fee878cf87d
lmas <lmasvensson@gmail.com>
2014-04-20 19:54:07 +0200

    Cleaned up entity movement for the future.

--- rocket_goth/main.lua
+++ rocket_goth/main.lua
@@ -1,5 +1,6 @@
 
 function love.load()
+    -- What could be cooler then a hamster? A hamster in a fucking ball!
     hamster = {
         image = love.graphics.newImage("hamster.png"),
         x = 50,
@@ -8,20 +9,35 @@ function love.load()
     }
 end
 
-function love.update(dt)
+function move_entity(entity, delta_x, delta_y)
+    -- Let an entity move around.
+    -- TODO: Collision checks
+    entity.x = entity.x + delta_x
+    entity.y = entity.y + delta_y
+end
+
+function player_control(entity, dt)
+    -- Put a player behind the wheel and let him drive some cool entity.
+    -- Calculate the movement deltas and then do the actual moving.
+    delta_x, delta_y = 0, 0
+
     if love.keyboard.isDown("right") then
-        hamster.x = hamster.x + (hamster.speed * dt)
-    end
-    if love.keyboard.isDown("left") then
-        hamster.x = hamster.x - (hamster.speed * dt)
+        delta_x = entity.speed * dt
+    elseif love.keyboard.isDown("left") then
+        delta_x = -entity.speed * dt
     end
 
     if love.keyboard.isDown("down") then
-        hamster.y = hamster.y + (hamster.speed * dt)
+        delta_y = entity.speed * dt
+    elseif love.keyboard.isDown("up") then
+        delta_y = - entity.speed * dt
     end
-    if love.keyboard.isDown("up") then
-        hamster.y = hamster.y - (hamster.speed * dt)
-    end
+
+    move_entity(entity, delta_x, delta_y)
+end
+
+function love.update(dt)
+    player_control(hamster, dt)
 end
 
 function love.draw()
